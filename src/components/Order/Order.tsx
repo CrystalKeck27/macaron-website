0adc4bf2259ec71d05c87ca91e370a9105f246ce
Crystal Keck <crystal.keck@abbott.com>
2022-08-10 10:46:50 -0500

    Added useEffect to load this month's flavors.

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -16,49 +16,64 @@ interface OrderFormValues {
 }
 
 function Order() {
-    const initialValues: OrderFormValues = {
-        macarons: [
-            {
-                name: "Macaron 1",
-                quantity: 0
-            },
-            {
-                name: "Macaron 2",
-                quantity: 0
-            },
-            {
-                name: "Macaron 3",
-                quantity: 0
-            },
-            {
-                name: "Macaron 4",
-                quantity: 0
-            }
-        ],
+    const macaronsNotLoaded: OrderFormValues = {
+        macarons: [],
         name: "",
         email: "",
         phone: ""
     };
+
+    const [initialValues, setInitialValues] = React.useState(macaronsNotLoaded);
+
+    React.useEffect(() => {
+        const macarons = [
+            {
+                name: "Cherry Berry Unicorn",
+                quantity: 0
+            },
+            {
+                name: "Blue Raspberry Rainbow",
+                quantity: 0
+            },
+            {
+                name: "Minecraft",
+                quantity: 0
+            },
+            {
+                name: "Vanilla",
+                quantity: 0
+            }
+        ];
+        //execute after 1 second
+        setTimeout(() => {
+            setInitialValues({
+                macarons: macarons,
+                name: "",
+                email: "",
+                phone: ""
+            });
+        }, 1000);
+    }, []);
 
     function onSubmit(values: OrderFormValues) {
         console.log(values);
     }
 
     return <div className="Order">
-        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Formik enableReinitialize={true} initialValues={initialValues} onSubmit={onSubmit}>
             {({values}) => (
                 <Form className='orderForm'>
                     <FieldArray name='macarons'>
                         {() => (
                             <div className='macaronSelection'>
-                                {values.macarons.length > 0 && values.macarons.map((macaron, index) => (
+                                {values.macarons.length > 0 ? values.macarons.map((macaron, index) => (
                                     <div className='macaronSpinner' key={index}>
                                         <label htmlFor={`macaron[${index}].quantity`}>{macaron.name}</label>
                                         <Field name={`macarons[${index}].quantity`} type='number' min='0' max='12'
                                                step='2'/>
                                         <ErrorMessage name={`macarons[${index}].quantity`} component='div'/>
                                     </div>
-                                ))}
+                                )) : <p>Loading this month's macarons...</p>}
                             </div>
                         )}
                     </FieldArray>
