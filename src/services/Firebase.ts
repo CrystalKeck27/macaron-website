acea334832503e7c0fa74f31715cfd5687925d8c
Crystal <45602803+CrystalKeck27@users.noreply.github.com>
2022-08-10 19:04:14 -0500

    Convert Firebase config to .env files

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
-// noinspection SpellCheckingInspection
 const firebaseConfig = {
-    apiKey: "AIzaSyBQ5CPz2SDIwEj_yEDThT6zTjdmwvji7sI",
-    authDomain: "macaron-website-dev.firebaseapp.com",
-    projectId: "macaron-website-dev",
-    storageBucket: "macaron-website-dev.appspot.com",
-    messagingSenderId: "539801592822",
-    appId: "1:539801592822:web:26fa54b2d82888e4ec1dd7",
-    measurementId: "G-YYWCLYNQE2"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 export const app = initializeApp(firebaseConfig);
